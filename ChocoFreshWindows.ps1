0df58d7589915475e8dc5293a4ce7aa1a08515ec
Ahmed AlSayed <ahmedsayedig@gmail.com>
2017-02-24 17:04:50 +1100

    Create ChocoFreshWindows ps1 script

---
+++ ChocoFreshWindows.ps1
@@ -0,0 +1,26 @@
+Set-ExecutionPolicy Unrestricted
+
+## Install Chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+## Install Desktop tools
+choco install powershell
+choco install googlechrome
+choco install flashplayerplugin
+choco install adobereader
+choco install firefox
+choco install 7zip
+choco install skype
+choco install notepadplusplus.install
+choco install vlc
+choco install paint.net
+choco install chocolateygui
+choco install teamviewer
+choco install itunes
+
+## Install Developer Tools
+choco install webdeploy
+choco install sourcetree
+choco install visualstudiocode
+choco install git
+choco install github
