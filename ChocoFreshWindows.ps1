6b48972e5b6b484cc394e3bbab3e7edc7a46c694
Ahmed ElSayed <ahmedsayedig@gmail.com>
2018-01-17 20:30:00 +1100

    Update ChocoFreshWindows.ps1

--- ChocoFreshWindows.ps1
+++ ChocoFreshWindows.ps1
@@ -1,27 +1,27 @@
-Set-ExecutionPolicy Unrestricted
+ Set-ExecutionPolicy Unrestricted
 
 ## Install Chocolatey
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 ## Install Desktop tools
-choco install powershell
-choco install googlechrome
-choco install flashplayerplugin
-choco install adobereader
-choco install firefox
-choco install 7zip
-choco install skype
-choco install notepadplusplus.install
-choco install vlc
-choco install paint.net
-choco install chocolateygui
-choco install teamviewer
-choco install itunes
-choco install whatsapp
+choco install powershell -y
+choco install googlechrome -y -y
+choco install flashplayerplugin -y
+choco install adobereader -y
+choco install firefox -y
+choco install 7zip -y
+choco install skype -y
+choco install notepadplusplus.install -y
+choco install vlc -y
+choco install paint.net -y
+choco install chocolateygui -y
+choco install teamviewer -y
+choco install itunes -y
+choco install whatsapp -y
 
 ## Install Developer Tools
-choco install webdeploy
-choco install git
-choco install github
-choco install sourcetree
-choco install visualstudiocode
+choco install webdeploy -y
+choco install git -y
+choco install github -y
+choco install sourcetree -y
+choco install visualstudiocode -y
